71209b91fc69a8076b732da7e1f7b70ac7f7000f
rkpatra201 <rkpatra201@gmail.com>
2017-08-31 01:05:58 +0530

    Add files via upload

---
+++ src/main/java/com/java/sample_jacoco_example/Calculator.java
@@ -0,0 +1,15 @@
+package com.java.sample_jacoco_example;
+
+public class Calculator {
+
+	public int sum(int a,int b)
+	{
+		return (a+b);
+	}
+	
+
+	public int diff(int a,int b)
+	{
+		return (a-b);
+	}
+}
---
+++ src/test/java/com/java/sample_jacoco_example/CalculatorTest.java
@@ -0,0 +1,29 @@
+package com.java.sample_jacoco_example;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+	Calculator cal;
+
+	@Before
+	public void before() {
+		cal = new Calculator();
+	}
+
+	@Test
+	public void sumTest() {
+		assertEquals(5, cal.sum(3, 2));
+	}
+
+	// @Test // uncommen this @Test line to make jacoco 100%.
+	// jacoco has an html file:
+	// file:///D:/workspace/programs/workbench/rabindra/sample-jacoco-example/target/site/jacoco/index.html
+	public void diffTest() {
+		assertEquals(1, cal.diff(3, 2));
+	}
+
+}
